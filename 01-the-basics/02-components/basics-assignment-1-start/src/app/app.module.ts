d674b658901744593d9e45f8a5b53cea532c1582
Venkatesh <Adithya.Venkatesh1@dell.com>
2023-09-12 12:14:53 -0400

    assignment 1 complete

--- 01-the-basics/02-components/basics-assignment-1-start/src/app/app.module.ts
+++ 01-the-basics/02-components/basics-assignment-1-start/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SuccessAlertComponent
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule,
